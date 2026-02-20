3443545414a15a7415171ca8f30679555a369500
Dmytro Shulha <dimonchik0218@gmail.com>
2026-02-20 23:51:33 +0200

    Merge pull request #1457 from pavlosoletskyy/patch-1 cinema visit done(changes)

--- app/main.py
+++ app/main.py
@@ -4,12 +4,21 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list[dict], hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+        movie: str,
+        customers: list[dict],
+        hall_number: int,
+        cleaner: str
+) -> None:
     customer_objs = []
     for cust_dict in customers:
-        c=Customer(name=cust_dict["name"], food=cust_dict["food"])
-        CinemaBar.sell_product(product=c.food, customer=c)
-        customer_objs.append(c)
+        cust = Customer(name=cust_dict["name"], food=cust_dict["food"])
+        CinemaBar.sell_product(product=cust.food, customer=cust)
+        customer_objs.append(cust)
     hall = CinemaHall(number=hall_number)
     cleaner_obj = Cleaner(name=cleaner)
-    hall.movie_session(movie_name=movie, customers=customer_objs, cleaning_staff=cleaner_obj)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objs,
+        cleaning_staff=cleaner_obj
+    )
